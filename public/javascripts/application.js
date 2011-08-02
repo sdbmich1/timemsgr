3342c323a643c23860fe6df3b11969e6e7b2c3ba
unknown <sdbmich@yahoo.com>
2011-08-02 14:30:28 -0700

    update js files to fix timer issue

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -74,32 +74,6 @@ $(document).ready(function (){
 	
 });
 
-$(function () {  
-  	var timerId = 0;
-	var qtimerId = 0;
-
-	$('#events').each(function() {
-    	timerId = setInterval(function(){
-      		$.ajax({ url: "/events/clock", type: "GET", dataType: "script"
-    		});
-  		}, 60000 );
-	});	
-	
- 	$('#quotes').each(function() {
-    	qtimerId = setInterval(function(){
-      		$.ajax({ url: "/events/getquote", type: "GET", dataType: "script"
-    		});
-  		}, 60000 );
-	});		
-	
-	$('.show-item, .manage-item').click(function () {  
- 		clearInterval(timerId);
- 		clearInterval(qtimerId);
-//    	return false;  
-  	});   
-  	
-});
-
 // add fancy box
 $(document).ready(function() {
 

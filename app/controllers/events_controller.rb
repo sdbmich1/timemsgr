15aaa11cc82a05ec9b688042dd12b330ae59a580
unknown <sdbmich@yahoo.com>
2012-01-16 08:34:22 -0800

    updated events controller for presenters logic

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,7 +7,7 @@ class EventsController < ApplicationController
  		@event = Event.find_event(params[:id], params[:etype], params[:sdate])
  		@sponsor_pages = @event.try(:sponsor_pages)
  		@notification = Notification.new
-    mobile_device? ? @presenters = @event.try(:presenters) : @presenters = @event.presenters.paginate(:page => params[:presenter_page], :per_page => 15)
+    mobile_device? ? @presenters = @event.try(:presenters) : @presenters = @event.presenters.paginate(:page => params[:presenter_page], :per_page => 15) if @event.presenters
 	end
 	
 	def index

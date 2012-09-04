a18b387d64c7f3b2f6f4da8cbaab4ecaf5d08569
unknown <sdbmich@yahoo.com>
2012-09-03 17:10:44 -0700

    added require to application rb for exception notifier

--- config/initializers/exception.rb
+++
@@ -1,4 +0,0 @@
-Timemsgr::Application.config.middleware.use ExceptionNotifier,
-  :email_prefix => "Koncierge-Errors: ",
-  :sender_address => %{"Koncierge Admin" <koncierge@rbca.net>},
-  :exception_recipients => %w{sdbmich1@gmail.com}

b5f184e0cb7738d2142dc96330ac06039c811070
unknown <sdbmich@yahoo.com>
2012-09-07 01:26:13 -0700

    update setup load paths for new rvm settings

--- config/setup_load_paths.rb
+++ config/setup_load_paths.rb
@@ -1,5 +1,7 @@
 if ENV['MY_RUBY_HOME'] && ENV['MY_RUBY_HOME'].include?('rvm')
   begin
+    gems_path = ENV['MY_RUBY_HOME'].split(/@/)[0].sub(/rubies/,'gems')
+    ENV['GEM_PATH'] = "#{gems_path}:#{gems_path}@koncierge"
     rvm_path     = File.dirname(File.dirname(ENV['MY_RUBY_HOME']))
     rvm_lib_path = File.join(rvm_path, 'lib')
 #    $LOAD_PATH.unshift rvm_lib_path

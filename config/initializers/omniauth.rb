e9a415145c656b291c193bdd7495ef4b9a7089b5
unknown <sdbmich@yahoo.com>
2013-01-16 23:53:25 -0800

    updated omniauth & devise rb files to fix facebook credentials problem

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do 
-  provider :facebook, API_KEYS['facebook']['api_key'], API_KEYS['facebook']['api_secret'], {:scope => 'email, user_birthday, user_events, user_interests, user_likes', :client_options => {:ssl => {:ca_path => "/etc/ssl/certs"} }} 
+#  provider :facebook, API_KEYS['facebook']['api_key'], API_KEYS['facebook']['api_secret'], {:scope => 'email, user_birthday, user_events, user_interests, user_likes', :client_options => {:ssl => {:ca_path => "/etc/ssl/certs"} }} 
   provider :openid, :store => OpenID::Store::Filesystem.new('/tmp')
 end

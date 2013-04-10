cae40759eece15bc2ddb185084c804d960efce18
Sean Brown <sdbmich@yahoo.com>
2013-04-10 13:58:59 -0700

    updated to updated resymlink call to add rm logic in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -59,7 +59,7 @@ namespace :deploy do
   desc "Recreate symlink"
   task :resymlink, :roles => :app do
     puts "\n\n=== Updating the symlink! ===\n\n"
-    run "ln -s #{release_path} #{current_path}"
+    run "rm -f #{current_path} && ln -s #{release_path} #{current_path}"
   end
 end
 
@@ -124,7 +124,7 @@ before 'deploy:setup', 'sphinx:create_sphinx_dir' #, 'rvm:install_rvm'
 # Sphinx
 #before 'deploy:update_code'
 after 'deploy:update_code', 'deploy:symlink_shared', 'sphinx:stop', "sphinx:sphinx_symlink", "sphinx:configure", "sphinx:rebuild"
-after "deploy:create_symlink", "rvm:trust_rvmrc", "whenever:update_crontab"
+after "deploy:create_symlink", "deploy:resymlink", "rvm:trust_rvmrc", "whenever:update_crontab"
 
 # Delayed Job  
 after "deploy:stop",    "delayed_job:stop"  
